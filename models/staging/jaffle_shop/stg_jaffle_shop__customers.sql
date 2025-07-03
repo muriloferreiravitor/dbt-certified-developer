a0d3b4a7de953513e7276aa1569cb1c6353e1e88
Murilo Ferreira Vitor <mferreiravitor@gmail.com>
2025-07-03 09:38:56 +0000

    docs: add model documentation and apply linting

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,6 +1,5 @@
 select
-    id as customer_id,
-    first_name,
-    last_name
-
+    id as customer_id
+    , first_name
+    , last_name
 from {{ source('jaffle_shop', 'customers') }}
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,11 +1,8 @@
 select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at
-
+    id as payment_id
+    , orderid as order_id
+    , paymentmethod as payment_method
+    , status
+    , amount / 100 as amount -- amount is stored in cents, convert it to dollars
+    , created as created_at
 from {{ source('stripe', 'payment') }}
